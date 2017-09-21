0cc2bf282ce0b2a2b4ab4edb62dc3ed0da48f24b
Raúl Torres <r.torresgonzalez.93@gmail.com>
2017-09-21 11:56:20 +0200

    no message

--- Assets/GameAssets/Scripts/Player.cs
+++ Assets/GameAssets/Scripts/Player.cs
@@ -1,8 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
+    [SyncVar(hook = "OnNameChange")]
+    string playerName;
+    [SyncVar(hook = "OnColorChange")]
+    Color playerColor;
 
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+
+        CmdChangeName("Raúl T.");
+        CmdChangeColor(Color.cyan);
+    }
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+
+        this.name = playerName;
+        this.GetComponentInChildren<Renderer>().material.color = playerColor;
+    }
+
+    void OnNameChange(string name)
+    {
+        this.name = name;
+    }
+    
+    void OnColorChange(Color color)
+    {
+        this.GetComponentInChildren<Renderer>().material.color = color;
+    }
+
+    [Command]
+    void CmdChangeName(string newName)
+    {
+        this.gameObject.name = newName;
+        // RpcChangeName(newName);
+        playerName = newName;
+    }
+
+    [Command]
+    void CmdChangeColor(Color newColor)
+    {
+        this.GetComponentInChildren<Renderer>().material.color = newColor;
+        // RpcChangeColor(newColor);
+        playerColor = newColor;
+    }
+
+    [ClientRpc]
+    void RpcChangeName(string newName)
+    {
+        this.gameObject.name = newName;
+    }
+
+    [ClientRpc]
+    void RpcChangeColor(Color newColor)
+    {
+        this.GetComponentInChildren<Renderer>().material.color = newColor;
+    }
 }
