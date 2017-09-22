94a49fb21be71955202602c79ea2529330dde753
Raúl Torres <r.torresgonzalez.93@gmail.com>
2017-09-22 11:44:58 +0200

    Agrega funcionalidad de comer a otro player, añade límites al escenario y coloca la cámara para que se vea todo el escenario

--- Assets/GameAssets/Scripts/Player.cs
+++ Assets/GameAssets/Scripts/Player.cs
@@ -3,64 +3,128 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
-public class Player : NetworkBehaviour
-{
-    [SyncVar(hook = "OnNameChange")]
+public class Player : NetworkBehaviour {
+
+    [SyncVar(hook = "OnNameChanged")]
     string playerName;
-    [SyncVar(hook = "OnColorChange")]
+
+    [SyncVar(hook = "OnColorChanged")]
     Color playerColor;
+
+    [SyncVar(hook = "OnRadioChanged")]
+    float playerRadio = 0.5f;
+    
+    Vector2 _lastDirection;
+    Rigidbody2D _rigidbody;
+
+    float GetArea()
+    {
+        return Mathf.PI * playerRadio * playerRadio;
+    }
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        playerRadio = Random.Range(3f, 4f);
+        OnRadioChanged(playerRadio);
+    }
+
+    public override void OnStartClient()
+    {        
+        base.OnStartClient();
+        if (!isLocalPlayer) { 
+            this.name = playerName;
+            this.GetComponentInChildren<Renderer>().material.color = playerColor;
+        }
+        OnRadioChanged(playerRadio);
+    }
 
     public override void OnStartLocalPlayer()
     {
         base.OnStartLocalPlayer();
 
+        this.name = "Raúl T.";
+        this.GetComponentInChildren<Renderer>().material.color = Color.yellow;
+
         CmdChangeName("Raúl T.");
-        CmdChangeColor(Color.cyan);
+        CmdChangeColor(Color.green);
     }
 
-    public override void OnStartClient()
+    private void Update()
+    {      
+        if(isLocalPlayer)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            Vector2 direction = new Vector2(
+                (horizontal > 0) ? 1 : (horizontal < 0) ? -1 : 0,
+                (vertical > 0) ? 1 : (vertical < 0) ? -1 : 0);
+            if(direction != _lastDirection)
+            {
+                _lastDirection = direction;
+                _rigidbody.velocity = direction.normalized * 15;                
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        base.OnStartClient();
+        if (isServer)
+        {
+            Player enemy = collision.attachedRigidbody.GetComponent<Player>();
+            if (this.playerRadio > enemy.playerRadio)
+            {
+                float distance = Vector3.Distance(this.transform.position, enemy.transform.position);
+                float sumRadios = this.playerRadio + enemy.playerRadio;
+                float radioToEat = sumRadios - distance;
+                radioToEat = Mathf.Clamp(radioToEat, 0, enemy.playerRadio);
 
-        this.name = playerName;
-        this.GetComponentInChildren<Renderer>().material.color = playerColor;
+                float currentEnemyArea = enemy.GetArea();
+                enemy.playerRadio -= radioToEat;
+                enemy.OnRadioChanged(enemy.playerRadio);
+
+                float newEnemyArea = enemy.GetArea();
+                float areaToAdd = currentEnemyArea - newEnemyArea;
+                float myNewArea = GetArea() + areaToAdd;
+                playerRadio = Mathf.Sqrt(myNewArea / Mathf.PI);
+                OnRadioChanged(playerRadio);
+            }
+        }        
     }
 
-    void OnNameChange(string name)
+    void OnNameChanged(string name)
     {
         this.name = name;
     }
-    
-    void OnColorChange(Color color)
+
+    void OnColorChanged(Color color)
     {
-        this.GetComponentInChildren<Renderer>().material.color = color;
+        GetComponentInChildren<Renderer>().material.color = color;
+    }
+
+    void OnRadioChanged(float radio)
+    {
+        this.transform.localScale = 2 * radio * Vector3.one;
     }
 
     [Command]
-    void CmdChangeName(string newName)
+    void CmdChangeName(string name)
     {
-        this.gameObject.name = newName;
-        // RpcChangeName(newName);
-        playerName = newName;
+        this.gameObject.name = name;
+        playerName = name;
     }
 
     [Command]
-    void CmdChangeColor(Color newColor)
+    void CmdChangeColor(Color color)
     {
-        this.GetComponentInChildren<Renderer>().material.color = newColor;
-        // RpcChangeColor(newColor);
-        playerColor = newColor;
-    }
+        GetComponentInChildren<Renderer>().material.color = color;
+        playerColor = color;
+    }    
 
-    [ClientRpc]
-    void RpcChangeName(string newName)
-    {
-        this.gameObject.name = newName;
-    }
 
-    [ClientRpc]
-    void RpcChangeColor(Color newColor)
-    {
-        this.GetComponentInChildren<Renderer>().material.color = newColor;
-    }
 }
---
+++ Assets/GameAssets/Scripts/SpawnPointGenerator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpawnPointGenerator : NetworkBehaviour {
+
+    [SerializeField] int nSpawnPoints = 20;
+    [SerializeField] Rect limits = new Rect(-100, -75, 200, 150);
+
+    void Start () {
+
+        if(isServer)
+        {
+            for (int i = 0; i < nSpawnPoints; i++)
+            {
+                GameObject newSpawnPoint = new GameObject("Spawn Point " + i);
+                newSpawnPoint.AddComponent<NetworkStartPosition>();
+                newSpawnPoint.transform.position = new Vector3(
+                    Random.Range(limits.xMin, limits.xMax),
+                    Random.Range(limits.yMin, limits.yMax),
+                    0);
+            }
+        }
+	}
+}
